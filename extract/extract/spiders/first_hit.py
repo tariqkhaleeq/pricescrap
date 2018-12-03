f494c88d45681deee3c8d20cb4587f5a5829c025
tariqkhaleeq <tariq.khaleeq@gmail.com>
2018-12-03 23:34:09 +0100

    For first_hit.py still trying to capture the correct data

---
+++ extract/extract/spiders/first_hit.py
@@ -0,0 +1,30 @@
+import scrapy
+
+class GetPrice(scrapy.Spider):
+	name="prices"
+	urls=[
+			'https://www.ambientedirect.com/en/categories/furniture'
+		]
+	# def initiate_requests(self):
+	# 	# Extract all furnitures and its prices. There are total of 5472 products in furniture alone
+		
+
+	# 	for url in urls:
+	# 		yield scrapy.Request(url=url, callback=self.parse)
+
+			# response.xpath('//div//div[@class="productSimpleTeaser__brand"]/text()').extract()
+
+	def parse(self, response):
+		for line in response.xpath('//div'):
+			yield {
+				# Find how to get category
+				'Company': line.xpath('//div//div//div//div[@class="productSimpleTeaser__name"]/text()').extract_first(),
+				'Brand': line.xpath('//div//div//div//div[@class="productSimpleTeaser__brand"]/text()').extract_first()
+				# 'Price':
+				# 'Discount':
+			}
+		# page = response.url.split("/")[-2]
+		filename = 'price.txt'
+		with open (filname, 'wb') as f:
+			f.write(response.body)
+		self.log('Saved file %s' % filename)
